d452ed119f8e54d5afbbacd6e48849763775cb7f
Mateusz Stadnik <matgla@live.com>
2023-11-21 17:25:17 +0100

    some changes

--- wallpaper/wallpaper_change.py
+++ wallpaper/wallpaper_change.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
-import argparse 
-import os 
+import argparse
+import os
 import random
 import subprocess
 from pathlib import Path
@@ -14,8 +14,16 @@ args, _ = parser.parse_known_args()
 wallpaper = random.choice(os.listdir(args.input))
 
 path = (Path(args.input) / wallpaper).absolute()
-subprocess.run("swww img " + str(path) + " --transition-step 200 --transition-fps 50 --transition-type center", shell=True)
+subprocess.run(
+    "swww img "
+    + str(path)
+    + " --transition-step 200 --transition-fps 50 --transition-type center",
+    shell=True,
+)
 
 subprocess.run("wal -c", shell=True)
-subprocess.run("wal -b '#000000' --cols16 --backend schemer2 --saturate 1.0  -i " + str(path), shell=True)
+subprocess.run(
+    "wal -b '#000000' --cols16 --backend schemer2 --saturate 1.0  -i " + str(path),
+    shell=True,
+)
 subprocess.run("eww reload", shell=True)
